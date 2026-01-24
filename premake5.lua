a7bc0f62e3e20d6c33ba2e44fb761b5b18439fb8
bighj256 <dongjiajundjj2006@qq.com>
2026-01-24 11:55:58 +0800

    使用静态运行时库

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "on"
+	staticruntime "on" --using static runtime
 	warnings "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
